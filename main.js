6b5f83d928bce42adaaa8bd183c3e13798de9559
Gerson Cesar Grobe de Miranda <106871453+cesargrobe@users.noreply.github.com>
2024-04-11 20:23:19 -0300

    Aula 6 - Finalizada

--- main.js
+++ main.js
@@ -1,4 +1,5 @@
 const botoes = document.querySelectorAll(".botao");
+const textos = document.querySelectorAll(".aba-conteudo");
 
 for (let i = 0; i < botoes.length; i++) {
     botoes[i].onclick = function () {
@@ -15,10 +16,12 @@ for (let i = 0; i < botoes.length; i++) {
 
 
 const contadores = document.querySelectorAll(".contador");
-const tempoObjetivo1 = new Date("2024-05-10T23:59:59");
+const tempoObjetivo1 = new Date("2024-04-30T23:59:59");
+const tempoObjetivo2 = new Date("2024-05-10T23:59:59");
+const tempoObjetivo3 = new Date("2024-06-15T23:59:59");
+const tempoObjetivo4 = new Date("2024-12-13T23:59:59");
 
-
-contadores[0].textContent = calculaTempo(tempoObjetivo1);
+const tempos = [tempoObjetivo1, tempoObjetivo2, tempoObjetivo3, tempoObjetivo4];
 
 function calculaTempo(tempoObjetivo) {
     let tempoAtual = new Date();
