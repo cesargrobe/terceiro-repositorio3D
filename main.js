30e6d06a50fbecb8b24787fe0c3bc85d733047d0
Gerson Cesar Grobe de Miranda <106871453+cesargrobe@users.noreply.github.com>
2024-04-16 22:44:24 -0300

    Aula 7 Finalizada

--- main.js
+++ main.js
@@ -37,5 +37,25 @@ function calculaTempo(tempoObjetivo) {
     minutos %= 60;
     horas %= 24;
 
-    return dias + " dias " + horas + " horas " + minutos + " minutos " + segundos + " segundos ";
+    if (tempoFinal > 0) {
+        return dias + " dias " + horas + " horas " + minutos + " minutos " + segundos + " segundos ";
+    } else {
+        return "Prazo finalizado";
+    }
+    
 }
+
+
+function atualizaCronometro() {
+    for ( let i = 0; i < contadores.length; i++) {
+        contadores[i].textContent = calculaTempo(tempos[i]);
+    }
+}
+
+function comecaCronometro(){
+    atualizaCronometro();
+    setInterval(atualizaCronometro(), 1000);
+}
+
+comecaCronometro();
+
